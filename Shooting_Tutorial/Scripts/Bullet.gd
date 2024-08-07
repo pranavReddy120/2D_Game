e6aef600d394f00652be4549bbfc066a7dec97d6
tangerine238 <stephenwtang23@gmail.com>
2024-08-06 21:30:36 -0400

    adding enemies

--- Shooting_Tutorial/Scripts/Bullet.gd
+++ Shooting_Tutorial/Scripts/Bullet.gd
@@ -4,3 +4,4 @@ var speed = 300
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+
---
+++ Shooting_Tutorial/Scripts/enemy.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const enemy = preload("res://Scenes/enemy.tscn")
+
+func enemy_creator():
+	randomize()
+	
+
+func _on_timer_timeout():
+	var rand_x = randi_range(-190,190)
+	var rand_y = randi_range(-105,105)
+	var enemy2 = enemy.instantiate()
+	get_parent().add_child(enemy2)
+	enemy2.global_position = Vector2(rand_x,rand_y)
+	$Timer.wait_time = 2
